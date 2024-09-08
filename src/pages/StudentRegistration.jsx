4d361a193886fcc177dfa944ef1f753abd5af73f
merwil vegas <com2pa@gmail.com>
2024-09-08 11:11:10 -0400

    plantillas base para el formulario de registro de estudiante

--- src/pages/StudentRegistration.jsx
+++ src/pages/StudentRegistration.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import SidebarWithHeader from '../pagesPrivate/LayoutPrivate/SidebarWithHeader';
-import { Button, ButtonGroup, Card, Flex, FormControl, FormLabel, Heading, Icon, IconButton, Input,  } from '@chakra-ui/react';
+import { Button, ButtonGroup, Flex,  Heading,  IconButton, } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
 import FormRegistrationStudent from './FormRegistrationStudent';
 
-export const StudentRegistration = () => {
+export const StudentRegistration = ({handleAddStudent}) => {
   const [numHijos ,setNumHijos]= useState(0);
 
   const [showCard, setShowCard] =  useState([]);
@@ -12,6 +12,8 @@ export const StudentRegistration = () => {
   const handleAddIconClick = () => {
     setShowCard([...showCard,true]);
   };
+
+
  
   return (
     <SidebarWithHeader>
@@ -21,72 +23,14 @@ export const StudentRegistration = () => {
       >
         <Heading>Inscripcion</Heading>
       </Flex>      
-      {/* <Card >
-        <FormControl>
-          <Flex flexDir={{base:'column',md:'row'}} p={{base:15}} gap={{base:4}}>
-           
-            <FormControl>
-              <Flex flexDir={{base:'column', }}>
-                <FormLabel fontSize={{base:12}} w="100%">Nombre Primer Niño/a </FormLabel>
-                <Input 
-                  placeholder="Ingrese nombre del alumno"
-                  size="sm"
-                />
-              </Flex>
-            </FormControl>
-            
-            
-            <FormControl>
-              <Flex flexDir={{base:'column'}}>
-                <FormLabel fontSize={{base:12}} w="100%">Apellido del Primer Niño/a</FormLabel>
-                <Input 
-                  placeholder="Ingrese apellido del alumno"
-                  size="sm" 
-                />
-              </Flex>
-            </FormControl>
-            
-          </Flex>         
-          <Flex flexDir={{base:'column',md:'row'}} p={{base:15}} gap={{base:4}}>
-            <FormControl>
-              <Flex flexDir={{base:'column',}}>
-                <FormLabel fontSize={{base:12}} w="100%">Sexo </FormLabel>
-                <Input 
-                  type='text' 
-                  placeholder="ingrese el sexo" 
-                  size="sm"    
-                />
-              </Flex>
-            </FormControl>
-           
-            <FormControl>
-              <Flex flexDir={{base:'column',}}>
-                <FormLabel fontSize={{base:12}} w="100%">Edad</FormLabel>
-                <Input 
-                  type='number' 
-                  placeholder="Ingrese edad del alumno" 
-                  size="sm"
-                />
-              </Flex>
-            </FormControl>
-          </Flex>
-          <Flex flexDir={{base:'column',md:'row'}} p={{base:15}} gap={{base:4}}>
-            <FormControl>
-              <Flex flexDir={'column'}>
-                <FormLabel 
-                  fontSize={{base:12}} 
-                  w="100%"
-                  display={'flex'}
-                  justifyContent={'center'}
-                >Grado del Primer niño/a</FormLabel>
-                <Input placeholder="ingrese el grado" />
-              </Flex>
-            </FormControl>
-          </Flex> 
-        </FormControl>
-      </Card> */}
+     
       {showCard.map((_, index)=>(
-        <FormRegistrationStudent key={index}/>
+        
+        <FormRegistrationStudent 
+          key={index}
+          handleAddStudent={handleAddStudent}
+        
+        />
       ))}
     
 
@@ -100,14 +44,7 @@ export const StudentRegistration = () => {
             onClick={handleAddIconClick}
           />
         </ButtonGroup>
-      </Flex>
-      <Flex 
-        justifyContent={{base:'center',md:'center'}}
-        p={{base:'1rem',md:'1rem'}}
-      >
-        <Button size="md" colorScheme="blue">Enviar</Button>
-      </Flex>
-        
+      </Flex>   
   
     </SidebarWithHeader>
   );

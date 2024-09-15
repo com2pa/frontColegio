0868566591e27b7eb4e08d9f05d38eba3dcfc45b
merwil vegas <com2pa@gmail.com>
2024-09-15 12:30:40 -0400

    muesto la lista de los estudiantes inscritos nombre apellido edad sexo

---
+++ src/pages/StudentList.jsx
@@ -0,0 +1,76 @@
+import { Button, Card,  Flex, Input } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
+export const StudentList = ({studentsRegistrations}) => {
+  const [isInputActive, setIsInputActive]=useState(false);
+  const [name, setName]=useState(studentsRegistrations.name);
+  const [lastname, setLastname]=useState(studentsRegistrations.lastname);
+  const [sex, setSex]=useState(studentsRegistrations.sex);
+  const [age,setAge]=useState(studentsRegistrations.age);
+  //   const [selectedDegree , setSelectedDegree] =useState(studentsRegistrations.selectedDegree);
+  return (
+    <Card
+      key={studentsRegistrations.id}
+      m={4}
+    >
+      <Flex w="100%" flexDir={{base:'column',md:'row'}}>
+        <Flex w="50%">
+          <Input
+            type='text'
+            readOnly={isInputActive ? true: false}
+            value={name}
+            borderWidth={isInputActive? '2px' : '0px'}
+            onChange={({target})=>setName(target.value)}
+          />
+        </Flex>
+        <Flex w="50%">
+          <Input
+            type='text'
+            readOnly={isInputActive? true: false}
+            value={lastname}
+            borderWidth={isInputActive? '2px' : '0px'}
+            onChange={({target})=>setLastname(target.value)}
+          />
+        </Flex>
+        
+
+
+            
+      </Flex>
+      <Flex w="100%" flexDir={{base:'column',md:'row'}}>
+        <Flex w="50%">
+          <Input
+            type='text'
+            readOnly={isInputActive ? true: false}
+            value={sex}
+            borderWidth={isInputActive? '2px' : '0px'}
+            onChange={({target})=>setSex(target.value)}
+          />
+        </Flex>
+        <Flex w="50%">
+          <Input
+            type='number'
+            readOnly={isInputActive? true: false}
+            value={age}
+            borderWidth={isInputActive? '2px' : '0px'}
+            onChange={({target})=>setAge(target.value)}
+          />
+        </Flex>
+        
+
+
+            
+      </Flex>
+      <Flex w="100%" flexDir={{base:'column',md:'row'}}>
+        {/* <CheckboxGroup>
+
+        </CheckboxGroup> */}
+            
+      </Flex>
+      
+
+    </Card>
+  );
+};
+
+export default StudentList;

1064db5b90daa9f55f22f56cccdc129c085a494f
merwil vegas <com2pa@gmail.com>
2024-09-15 12:31:55 -0400

    se mostrara ingrese de cronograma de actividades por materia grado y lapso

---
+++ src/pages/Assignments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SidebarWithHeader from '../pagesPrivate/LayoutPrivate/SidebarWithHeader';
+
+export const Assignments = () => {
+  return (
+    <SidebarWithHeader>
+        
+    </SidebarWithHeader>
+  );
+};
+
+export default Assignments;

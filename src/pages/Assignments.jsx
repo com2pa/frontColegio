d234f0f5340a738fca915389b5ca02f200dfd3ac
merwil vegas <com2pa@gmail.com>
2024-09-26 10:23:14 -0400

    pagina para crear todas las asignaciones

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -3,19 +3,29 @@
 // lapso, grado, materia, nombre de la actividad , que tipo de actividad 
 import React, { useState } from 'react';
 import SidebarWithHeader from '../pagesPrivate/LayoutPrivate/SidebarWithHeader';
-import { Box, Button, Card, Checkbox, CheckboxGroup, Flex, FormControl, FormHelperText, FormLabel, Heading, Img, Input, Select, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Card, Checkbox, CheckboxGroup, Flex, FormControl, FormHelperText, FormLabel, Heading, Img, Input, Select, Stack, Text, useToast } from '@chakra-ui/react';
 import dos from '../assets/2.png';
 import { useEffect } from 'react';
 import axios from 'axios';
+import CardAssignments from './CardAssignments';
 
 
 export const Assignments = () => {
     
   const [degree, setDegree]=useState([ ]);
-  const [selectedDegree, setSelectedDegree] =useState(null);
-  const [subject, setSubject]=useState([]);
-  const [selectedLapso ,setSelectedLapso] =useState('');
+  const [selectedDegree, setSelectedDegree] =useState('');
 
+  const [lapso ,setLapso] =useState('');
+
+  const [subject, setSubject]=useState([]);
+  const [subjectSelected, setsubjectSelected]=useState('');
+
+  const [name, setName]=useState('');
+
+  const [tipo, setTipo]=useState('');
+  const toast=useToast();
+
+  const[assignment, setAssignment]= useState([]);
 
 
   
@@ -40,11 +50,91 @@ export const Assignments = () => {
 
   //   obtener el valor de grado al seleccionarlo
   const handleDegreeChange = (degree) => {
-    setSelectedLapso(degree);
-    console.log('seleccionando el grado !!',degree);
+    setSelectedDegree(degree);
+    // console.log('seleccionando el grado !!',degree);
   };
 
+  // obtener el lapso seleccionado
+  const handleSelectLapso=(lapso)=>{
+    setLapso(lapso);
+    // console.log('seleccionando el lapso!!',lapso);
+  };
+  
+  // mostrando todas las asignaciones
+  useEffect(()=>{
+    const fetchAssignments=async()=>{
+      const response = await axios.get('/api/assignment');
+      setAssignment(response.data);
+      // console.log(response.data);
+    };
+    fetchAssignments();
+  },[setAssignment]);
+ 
 
+  // enviar 
+  const handleSubmit = async () => {
+    // e.preventDefault();
+    // console.log('Selected Subject:', subjectSelected);
+    try {
+      const {data}= await axios.post('/api/assignment', {
+        name,
+        tipo,
+        lapso,
+        selectedDegree,
+        subjectSelected,
+      });
+      // console.log(data);
+      toast({
+        title: 'Actividad agregada correctamente',
+        description: data.selectedDegree,
+        status:'success',
+        isClosable:true,
+        duration: 3000,
+
+
+      });
+      // alert('Actividad agregada exitosamente');
+      setName('');
+      setTipo('');
+      setSelectedDegree('');
+      setsubjectSelected('');
+      setLapso('');
+
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: 'Error al agregar actividad',
+        description: error.response.data.error,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+  // eliminar la asignatura
+  const handleDelete=async(id)=>{
+    try {
+      await axios.delete(`/api/assignment/${id}`);
+      toast({
+        title: 'Actividad eliminada correctamente',
+        description: 'La actividad ha sido eliminada exitosamente',
+        status:'success',
+        isClosable:true,
+        duration: 3000,
+      });
+      // alert('Actividad eliminada correctamente');
+      setAssignment(assignment.filter(item => item._id!== id));
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: 'Error al eliminar actividad',
+        description: error.response.data.error,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
 
 
   return (
@@ -86,8 +176,8 @@ export const Assignments = () => {
                 <FormLabel>Lapso</FormLabel>
                 <Select 
                   placeholder='Seleccione'
-                  value={selectedLapso}
-                  onClick={handleDegreeChange}
+                  value={lapso}
+                  onChange={(e)=>handleSelectLapso(e.target.value)}
                 >
                   <option value='1er lapso'>1er lapso</option>
                   <option value='2do lapso'>2do lapso</option>
@@ -107,13 +197,14 @@ export const Assignments = () => {
                 <CheckboxGroup
                   defaultValue={degree.id}                  
                   colorScheme='green'
-                //   onChange={handleSelectDegree}
+                  onChange={ handleDegreeChange}
                 >
                   <Stack spacing={5} direction='row'>
                     {degree.map((degree) => (
                       <Checkbox 
                         key={degree.id} 
                         value={degree.id}
+                        
                         size='sm' 
                         colorScheme='red'
                         defaultChecked                      
@@ -140,8 +231,12 @@ export const Assignments = () => {
             >
               <FormControl>
                 <FormLabel>Asignatura</FormLabel>
-                {/* <Input type='email' /> */}
-                <Select placeholder='Seleccione'>
+                <Select 
+                  placeholder='Seleccione'
+                  // value={subjectSelected}
+                  // onChange={(e)=>handleSelectSubject(e.target.value)}
+                  onChange={({target})=>setsubjectSelected(target.value)}
+                >
                   {subject.map((subject) => (
                     <option key={subject.id} value={subject.id}>{subject.name}</option>
                   ))}                  
@@ -155,7 +250,11 @@ export const Assignments = () => {
             >
               <FormControl>
                 <FormLabel>Tema(de la actividad)</FormLabel>
-                <Input type='email' />
+                <Input 
+                  type='text'
+                  value={name}
+                  onChange={(e)=>setName(e.target.value)}
+                />
                 {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
               </FormControl>
             </Flex>
@@ -173,15 +272,61 @@ export const Assignments = () => {
             >
               <FormControl>
                 <FormLabel>actidades</FormLabel>
-                <Input type='email' />                
+                <Input 
+                  type='text'
+                  value={tipo}
+                  onChange={(e)=>setTipo(e.target.value)} 
+                />                
               </FormControl>
             </Flex>
           </Flex>
         </FormControl>
-        <Button variant="solid" size="md" colorScheme="blue">
+        <Button 
+          variant="solid" 
+          size="md" 
+          colorScheme="blue"
+          // onClick={()=>
+          //   console.log('creando actividad!!',
+          //     lapso,
+          //     name,
+          //     tipo,              
+          //     selectedDegree
+          //     selectedSuject,
+
+          //   )}
+          onClick={handleSubmit}
+          isDisabled={ 
+            !name || 
+            !tipo  || 
+            !lapso || 
+            !subjectSelected ||
+            !selectedDegree
+          }
+        >
           Create
         </Button>
       </Card>
+      <Flex>
+        <Box w='100%' p={8}>
+          <Heading>Asignaciones</Heading>
+          
+          {assignment.map((assignment) => (
+            // <Card key={assignment.id}>
+            //   <Text>
+            //     Grado: {assignment.degree} - Lapso: {assignment.lapso} - Asignatura: {assignment.subject} - Tema: {assignment.name} - Actidad: {assignment.tipo}
+            //   </Text>
+            // </Card>
+            <CardAssignments
+              key={assignment.id}
+              assignment={assignment}
+              handleDelete={handleDelete}
+                
+
+            />
+          ))}
+          
+        </Box>
+      </Flex>
         
     </SidebarWithHeader>
   );

b86a87cf33a3402a7db3517739cd3f0fa8fc2bfe
merwil vegas <com2pa@gmail.com>
2024-11-06 18:12:22 -0400

    se añadio responsive

--- src/pages/TabletStudentsDegrees.jsx
+++ src/pages/TabletStudentsDegrees.jsx
@@ -1,14 +1,12 @@
 // tabla  de estudiante por grado
 import { Card,    
-  Table, 
-  TableContainer, 
+  Table,
   Tbody, 
   Td,  
   Tfoot, 
   Th, 
   Thead, 
   Tr, 
-  // useToast 
 } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
@@ -18,8 +16,7 @@ export const TabletStudentsDegrees = ({degree}) => {
   const [
     // assignments, 
     setAssignments] = useState([]);
-  // const [selectedDegree, setSelectedDegree] = useState(null);
-  // const toast = useToast();
+
 
   // obteniendo todas las asignaciones
   useEffect(() => {
@@ -30,81 +27,58 @@ export const TabletStudentsDegrees = ({degree}) => {
         // console.log(response.data);
       } catch (error) {
         console.log(error);
+    
       }
     };
 
     FetchAssignments(degree.id);
-  }, [degree.id, setAssignments]);
+  }, [degree.id, setAssignments, ]);
 
-  //   seleccionar el grado
-  // const handleSelectDegree=(degree)=>{
-  //   setSelectedDegree(degree);
-  //   // console.log('grado seleccionado !',degree.degree, degree.students);
-  //   toast({
-  //     title: `Estudiantes del ${degree.degree}° grado`,
-  //     description: 'cargado grados',
-  //     status: 'info',
-  //     duration: 1000,
-  //     isClosable: true,
-  //   });
-  // };
-
+ 
   return (
     <>
-      <Card>           
-        <TableContainer 
-        
+      <Card>
+        <Table 
+          size='sm'
+          variant="striped" 
+          colorScheme="gray"
+          borderCollapse="collapse"
+          width={{ base: '100%', md: 'auto' }}
+                  
         >
-          <Table 
-            size='sm' 
-            display="block"
-            variant="striped" 
-            colorScheme="gray"
-            borderCollapse="collapse"
-            width={{ base: '100%', md: 'auto' }}
+          <Thead
+            bgGradient={'linear(to-l, #6066FA, yellow)'}
           >
-            <Thead>
-              <Tr>
-                <Th>N°</Th>
-                <Th>Nombre y Apellido del estudiante </Th>
-                {/* {degree.assignments && degree.assignments.length > 0 ? (
-                  assignments.map((assignment, index) => (
-                    <>
-                      <Th key={index}>{assignment.lapso} {assignment.name}</Th>
-                    </>
-                  ))
-                ) : null} */}
-                {/* {assignments.length > 0 ? (
-                  assignments.map((assignment,) => (
-                    <>
-                      <Th >{assignment.lapso} {assignment.name}</Th>
-                    </>
-                  ))
-                ) : null} */}
-                            
-              </Tr>
-            </Thead>
-            {/* mostrando los alumnos por grado  */}
-            <Tbody>
-              {degree.students.map((student, index) => (
-                <Tr key={student.id}>
-                  <Td>{index + 1}</Td>
-                  <Td>{student.name} {student.lastname}</Td>              
+            <Tr>
+              <Th>N°</Th>
+              <Th>Nombre y Apellido del estudiante </Th>
+            </Tr>
+          </Thead>
+          {/* mostrando los alumnos por grado  */}
+          <Tbody>
+            {degree.students.map((student, index) => (
+              <Tr key={student.id}>
+                <Td>{index + 1}</Td>
+                <Td>{student.name} {student.lastname}</Td>              
                  
-                </Tr>
-              ))
-              }              
-                     
-            </Tbody>
-            <Tfoot>
-              <Tr>                
-                <Th>Coligio casa de niños</Th>
               </Tr>
-            </Tfoot>
-          </Table> 
-        </TableContainer> 
+            ))
+            }              
+                     
+          </Tbody>
+          <Tfoot>
+            <Tr>                
+              <Th
+                colSpan="3"
+                fontWeight='bold'
+                fontSize='md'
+                textAlign='center'
+                color='black.600'
+              >Coligio casa de niños</Th>
+            </Tr>
+          </Tfoot>
+        </Table> 
       </Card>
-      
     </>
   );
 };

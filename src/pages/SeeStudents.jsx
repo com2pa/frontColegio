d37a0a6687ff6fbf3d841a2319f1ea7df22104db
merwil vegas <com2pa@gmail.com>
2024-11-06 18:11:54 -0400

    se añadio responsive

--- src/pages/SeeStudents.jsx
+++ src/pages/SeeStudents.jsx
@@ -12,17 +12,15 @@ import TabletStudentsDegrees from './TabletStudentsDegrees';
 
 export const SeeStudents = () => {
   const [ degree , setDegree]=useState([]);
-
   const [selectedDegree, setSelectedDegree] = useState(null);
   const toast=useToast();  
-
 
   // mostrar todos los grados
   useEffect(()=>{
     const fetchDegree =async()=>{
       const {data} = await axios.get('/api/degrees');
       setDegree(data);
-      console.log(data);
+      // console.log(data);
     };
     fetchDegree();  
   },[setDegree]);
@@ -46,47 +44,30 @@ export const SeeStudents = () => {
 
   return (
     <SidebarWithHeader>
-      <h1>Aquí se verán los estudiantes</h1>
-      
       <Grid 
-        templateColumns="repeat(3, 1fr)" 
+        templateColumns="repeat(3, 1fr)"
         gap={4}
+        gridTemplateColumns={["auto auto "]} 
+        mb={4}       
       >
         {degree.map((degree) => (
           <GridItem key={degree._id}>
-            
             <Card 
-              h={{md:'10vh'}}              
+                        
               bg='gray.200'
               onChange={handleSelectDegree}
-            //   onClick={() => setSelectedDegree(degree._id)}
             >
               <Flex
                 w="100%"
-                // gap={12}
                 flexDir="column"
-                justifyContent="space-evenly"
+                gap={4}
               >
                 <Flex 
                   justifyContent={'center'}                
                 >
                   <Text 
                     as='b'
-
                   >{degree.degree}° grado</Text>
-                  {/* {degree.students && degree.students.length > 0 ? (
-                  <List>
-                    {degree.students.map((student) => (
-                      <ListItem key={student._id}>
-                       c
-                      </ListItem>
- 
-                    ))}
-                  </List>
-                ) : (
-                  <Text>No hay estudiantes en este grado.</Text>
-                )}            */}
-
                 </Flex>
                 <Flex
                   justifyContent={'center'}
@@ -112,22 +93,10 @@ export const SeeStudents = () => {
 
           <Card 
             w={"100%"} 
-            my={8}
-            h={'auto'}
+            my={8}            
+            p={4}
           >            
-            <Flex
-              my={8}
-              justifyContent={'center'}
-            >                
-              <Input         
-                size='md'             
-                borderColor='gray.200'             
-                rounded='md'              
-                _placeholder={{ color: 'gray.400' }}              
-                w={"50%"}
-                placeholder='Buscar estudiante...' 
-                type='search'/>
-            </Flex>
+            
             <Box w={"100%"}>
               <Text 
                 as='h2'

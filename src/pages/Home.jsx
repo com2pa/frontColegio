2bbd5b70121f8ee0cd5e3e3561a23b15cad0db8d
merwil vegas <com2pa@gmail.com>
2024-11-07 10:23:53 -0400

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,16 +24,35 @@ export const Home = () => {
                          
             </Flex>
           </Card>
-          <Card variant="outline" width="100%" height="30rem" display={{base: 'none', md: 'block'}}>
+          <Card 
+            variant="outline" 
+            width="100%" 
+            height="100%" 
+            display={{base: 'column', md: 'block'}}
+          >
             <Image 
-              src={Educacion} 
+              src={Educacion}               
               alt="educacion" 
-              height="100%" 
+              // height={'100%'} 
               objectFit="cover" 
               w="100%"  /> 
           </Card>
         </Flex> 
         {/* seccion 2 */}
+        {/* <Flex gap="1rem" flexDir={{base: 'column', md: 'row'}}  >
+          <Flex flexDir={{ base: "column", md: 'row'}}  w="100%" gap={4} align="center">
+            <Card p={4} display="flex" variant="outline" w="100%" height="100%" flexDir="column" gap={4} justifyContent="center">
+              <Text color="red.600" fontSize={{ base: 25, md: 45}} fontWeight="600">
+                Actividades
+               
+              </Text>
+              <Text>
+                
+                
+              </Text>
+            </Card>
+          </Flex>
+        </Flex> */}
         
       
       
